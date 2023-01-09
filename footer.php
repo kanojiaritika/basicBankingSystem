fe65178bd2e7421059bd6dca6200e83058bc200d
rituKanojia <116656375+rituKanojia@users.noreply.github.com>
2023-01-09 22:58:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Document</title>
+</head>
+<style>
+  .custom-shape-divider-bottom-1673187063 {
+  position: absolute;
+  width: 100%;
+  margin-top: -90px;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1673187063 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 93px;
+}
+
+.custom-shape-divider-bottom-1673187063 .shape-fill {
+  fill: rgb(36, 1, 68);
+}
+
+.custom-shape-divider-top-1673186983 {
+  position: absolute;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+}
+
+.custom-shape-divider-top-1673186983 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 93px;
+}
+
+.custom-shape-divider-top-1673186983 .shape-fill {
+  fill: rgb(36, 1, 68);
+}
+
+footer {
+  text-align: center;
+  padding: 80px 0px 40px 0px;
+  background-image: linear-gradient(rgb(59, 0, 111), rgb(159, 104, 206));
+}
+footer p {
+  font-size: 20px;
+}
+footer ul {
+  display: flex;
+  margin-left: 550px;
+}
+footer ul li {
+  list-style: none;
+}
+footer ul li a {
+  color: white;
+  padding: 0px 30px;
+}
+footer ul li a:hover {
+  color: black;
+  transition: 0.4s;
+}
+
+</style>
+<body>
+<div class="custom-shape-divider-bottom-1673187063">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
+<div class="custom-shape-divider-top-1673186983">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
+
+
+
+<footer>
+      <h1>Secure Bank<h1>
+      <p>Secure bank is used to transfer money from customer A to customer B. It also provides details of all the customers and transaction history.</p>
+      <ul>
+        <li><a href="#"><i class ="fa fa-facebook"></i></a></li>
+        <li><a href="#"><i class ="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class ="fa fa-linkedin-square"></i></a></li>
+        <li><a href="#"><i class ="fa fa-instagram"></i></a></li>
+      </ul>
+      <p>Copyright &copy 2023 SecureBank. Developed By Ritika Kanojia</p>
+  </footer>
+</body>
+</html>
---
+++ selectuserdetail.php
@@ -0,0 +1,174 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['submit']))
+{
+    $from = $_GET['id'];
+    $to = $_POST['to'];
+    $amount = $_POST['amount'];
+
+    $sql = "SELECT * from customers where id=$from";
+    $query = mysqli_query($conn,$sql);
+    $sql1 = mysqli_fetch_array($query); // returns array or output of user from which the amount is to be transferred.
+
+    $sql = "SELECT * from customers where id=$to";
+    $query = mysqli_query($conn,$sql);
+    $sql2 = mysqli_fetch_array($query);
+
+
+
+    // constraint to check input of negative value by user
+    if (($amount)<0)
+   {
+        echo '<script type="text/javascript">';
+        echo ' alert("Oops! Negative values cannot be transferred")';  // showing an alert box.
+        echo '</script>';
+    }
+
+
+  
+    // constraint to check insufficient balance.
+    else if($amount > $sql1['balance']) 
+    {
+        
+        echo '<script type="text/javascript">';
+        echo ' alert("Bad Luck! Insufficient Balance")';  // showing an alert box.
+        echo '</script>';
+    }
+    
+
+
+    // constraint to check zero values
+    else if($amount == 0){
+
+         echo "<script type='text/javascript'>";
+         echo "alert('Oops! Zero value cannot be transferred')";
+         echo "</script>";
+     }
+
+
+    else {
+        //Changed all B to b change again if problem
+                // deducting amount from sender's account
+                $newbalance = $sql1['balance'] - $amount;
+                $sql = "UPDATE customers set balance=$newbalance where id=$from";
+                mysqli_query($conn,$sql);
+             
+
+                // adding amount to reciever's account
+                $newbalance = $sql2['balance'] + $amount;
+                $sql = "UPDATE customers set balance=$newbalance where id=$to";
+                mysqli_query($conn,$sql);
+                
+                $sender = $sql1['name'];
+                $receiver = $sql2['name'];
+                $sql = "INSERT INTO transaction(`sender`, `receiver`, `balance`) VALUES ('$sender','$receiver','$amount')";
+                $query=mysqli_query($conn,$sql);
+
+                if($query){
+                     echo "<script> alert('Transaction Successful');
+                                     window.location='transactionhistory.php';
+                           </script>";
+                    
+                }
+
+                $newbalance= 0;
+                $amount =0;
+        }
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction</title>
+    <link rel="stylesheet" href="selectUser.css">
+</head>
+
+<body>
+<header>
+    <h1>Secure Bank</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="transfermoney.php">Transfer Money</a></li>
+          <li><a href="transactionhistory.php">Transaction History</a></li>
+        </ul>
+      </nav>
+</header>
+
+	<div class="container">
+        <h2>Transaction</h2>
+            <?php
+                include 'config.php';
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM  customers where id=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error : ".$sql."<br>".mysqli_error($conn);
+                }
+                $rows=mysqli_fetch_assoc($result);
+            ?>
+            <form method="post" name="tcredit" class="tabletext" ><br>
+        <div>
+            <table>
+                <tr style="color : black;">
+                    <th >Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Balance</th>
+                </tr>
+                <tr>
+                    <td><?php echo $rows['id'] ?></td>
+                    <td><?php echo $rows['name'] ?></td>
+                    <td><?php echo $rows['email'] ?></td>
+                    <td><?php echo $rows['balance'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <br><br>
+        <label class="transfer-to"><b>Transfer To:</b></label>
+        <select name="to" required>
+            <option value=" " disabled selected>Choose</option>
+            <?php
+                include 'config.php';
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM customers where id!=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error ".$sql."<br>".mysqli_error($conn);
+                }
+                while($rows = mysqli_fetch_assoc($result)) {
+            ?>
+                <option class="table" value="<?php echo $rows['id'];?>" >
+                
+                    <?php echo $rows['name'] ;?> (Balance: 
+                    <?php echo $rows['balance'] ;?> ) 
+               
+                </option>
+            <?php 
+                } 
+            ?>
+            
+            <div>
+        </select>
+        <br>
+        <br>
+            <label class="amount"><b>Amount:</b></label>
+            <input type="number" class="form-control" name="amount" required>   
+            <br><br>
+                <div>
+            <button name="submit" type="submit" id="myBt" class="transfer-button">TRANSFER</button>
+            </div>
+        </form>
+    </div>
+    <?php
+        include 'footer.php';
+    ?>
+</body>
+</html>
